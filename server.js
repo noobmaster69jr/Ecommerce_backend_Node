f0c19a1a56b14de77c94226e9c50c5020e54ac46
noobmaster69jr <srashwin22@gmail.com>
2023-12-22 10:24:54 +0530

    added body parser

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const {PORT}  = require('./config/serverConfig')
 const categoryRoutes = require('./routes/category.routes')
 const app = express()
 
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 categoryRoutes(app)  // passing app (express)
 
